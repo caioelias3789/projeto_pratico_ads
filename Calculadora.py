07488e3ff3dde6c3039cd66717848fdef15cad5f
Caio Elias <caioelias3789@gmail.com>
2025-05-30 15:36:56 -0300

    Estrutura inicial da calculadora

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+#nosso projeto em python
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+app = ctk.CTk()
+app.geometry("300x300")
+app.title("Calculadora")
+app.mainloop()
